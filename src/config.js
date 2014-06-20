eb4b313a03a1949c3439c47816cf2262cf2600c0
Ken Bellows <ken.bellows@live.com>
2014-06-20 05:39:30 -0400

    setting up some initial files

---
+++ src/config.js
@@ -0,0 +1,11 @@
+/* Config.js
+ *
+ * Configure the canvas, initialize certain global variables, etc.
+ *
+ * Kept separate from globals.js to conceptually separate the acts of
+ * defining global variables and setting global configurations.
+ */
+
+canvas.width  = 900;
+canvas.height = 900;
+
---
+++ src/globals.js
@@ -0,0 +1,10 @@
+/* Globals.js
+ *
+ * Define global variables to be used throughout the rest of the
+ * application. Herein is defined the canvas, its context, certain
+ * game vars, etc. Some of these will be modified in config.js.
+ */
+
+var canvas = document.getElementByTagName("canvas"),
+    ctx    = canvas.getContext('2d');
+
